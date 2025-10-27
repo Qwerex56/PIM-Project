80837cbc89d05b6d4591ebff0237fce9255c6f36
Adrian Czubaty <272756@student.pwr.edu.pl>
2025-10-27 15:57:49 +0100

    Horizontal weather card component

---
+++ weather_app/src/pages/TestPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    <horizontal-weather-card
+      :weather-data="{
+        LocationName: 'San Francisco',
+        TemperatureMax: 22,
+        TemperatureMin: 15,
+        Condition: 'Sunny',
+        ConditionIconUrl: '//cdn.weatherapi.com/weather/128x128/day/176.png'
+      }"
+    ></horizontal-weather-card>
+  </q-page>
+</template>
+
+<script setup lang="ts">
+import HorizontalWeatherCard from 'components/HorizontalWeatherCard/HorizontalWeatherCard.vue';
+</script>
