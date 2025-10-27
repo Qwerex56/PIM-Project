0899be886dd4ab3cf4fa6cad8d2e521a1eaf6b4f
Adrian Czubaty <272756@student.pwr.edu.pl>
2025-10-27 16:23:18 +0100

    Shaped icon - installed font awesome package

---
+++ weather_app/src/components/shaped-icon/ShapedIcon.vue
@@ -0,0 +1,22 @@
+<template>
+<div class="shaped-icon">
+  <font-awesome-icon icon="fa-solid {{ props.icon?? 'fa-circle' }}" width-auto/>
+</div>
+</template>
+
+<script setup lang="ts">
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+const props = defineProps<{
+  icon?: string
+}>();
+
+</script>
+
+<style scoped lang="css">
+.shaped-icon {
+  padding: .375rem;
+
+  border-radius: 9999px;
+}
+</style>
