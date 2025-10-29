4471c926b73df7ef258bd96a07a3aed464e326ea
Maciej Sikorski <maciejosikorski@gmail.com>
2025-10-29 08:23:16 +0100

    Add status card structure
    Signed-off-by: Maciej Sikorski <maciejosikorski@gmail.com>

---
+++ weather_app/src/components/status-card/StatusCard.vue
@@ -0,0 +1,10 @@
+<template>
+  <QCard>
+    <QCardSection>
+      <div class="status-card">
+        <h2>Status Card</h2>
+        <p>This is a status card component.</p>
+      </div>
+    </QCardSection>
+  </QCard>
+</template>
